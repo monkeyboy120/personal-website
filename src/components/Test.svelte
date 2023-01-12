9d2cd67cfb7ff369a80829955e88f598730e3f91
Joshua La <j0shua001@outlook.com>
2023-01-11 23:56:49 -0500

    Tailwind + centered button

--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -5,4 +5,32 @@
     };
 </script>
 
-<button on:click = {increment}>{count}</button>
+<div><button on:click = {increment}>{count}</button></div>
+
+<style>
+    div {
+        background-color: antiquewhite;
+        display: flex;
+        width: 100%;
+        height: 100vh;
+        justify-items: center;
+        align-items: center;
+        justify-content: center;
+        align-content: center;
+    }
+
+    button {
+        border-radius: 10px;
+        height: 100px;
+        width: 100px;
+        background-color: aqua;
+    }
+
+    button:hover {
+        box-shadow: 10px red;
+    }
+
+    button:active {
+        background-color: blueviolet;
+    }
+</style>
