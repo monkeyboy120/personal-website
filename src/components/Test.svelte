7933474972e2ed86937a2036379a217c8b2dba19
Joshua La <j0shua001@outlook.com>
2023-01-16 23:50:55 -0500

    Refactored navbar into tailwind

--- src/components/Test.svelte
+++ src/components/Test.svelte
@@ -1,37 +1,39 @@
 <script>
-    let count = 0;
-    function increment() {
-        count++
-    };
+  let count = 0;
+  function increment() {
+    count++;
+  }
 </script>
+
 {#if count > 10}
-    <div><button on:click = {increment}>You clicked {count} times</button></div>
+  <div><button on:click={increment}>You clicked {count} times</button></div>
 {:else}
-<div><button on:click = {increment}>{count}</button></div>
+  <div><button on:click={increment}>{count}</button></div>
 {/if}
+
 <style>
-    div {
-        background-color: black;
-        display: flex;
-        width: 100vw;
-        height: 100vh;
-        justify-items: center;
-        align-items: center;
-        justify-content: center;
-        align-content: center;
-    }
+  div {
+    background-color: black;
+    display: flex;
+    width: 100vw;
+    height: 100vh;
+    justify-items: center;
+    align-items: center;
+    justify-content: center;
+    align-content: center;
+  }
 
-    button {
-        border-radius: 10px;
-        height: 100px;
-        width: 100px;
-        background-color: aqua;
-    }
+  button {
+    border-radius: 10px;
+    height: 100px;
+    width: 100px;
+    background-color: aqua;
+  }
 
-    button:hover {
-        background-color: aquamarine;
-    }
-    button:active {
-        background-color: blueviolet;
-    }
+  button:hover {
+    background-color: aquamarine;
+  }
+  button:active {
+    background-color: blueviolet;
+  }
 </style>
