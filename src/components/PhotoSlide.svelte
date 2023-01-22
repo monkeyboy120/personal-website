87e50ae2629b3dbfd8f96abf420be27f723c9bb3
Joshua La <j0shua001@outlook.com>
2023-01-22 13:39:38 -0500

    Made slideshow move

--- src/components/PhotoSlide.svelte
+++ src/components/PhotoSlide.svelte
@@ -1,39 +1,38 @@
 <script>
   let track;
-  let mouseDownAt = 0;
-  let prevPercentage = 0;
-  let percentage = 0;
+  let innerWidth;
 
-  const handleOnDown = (e) => {
-    mouseDownAt = e.clientX;
-  };
+  const handleOnDown = (e) => (track.dataset.mouseDownAt = e.clientX);
 
   const handleOnUp = () => {
-    mouseDownAt = 0;
-    prevPercentage = percentage;
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage;
   };
 
   const handleOnMove = (e) => {
-    if (mouseDownAt === 0) return;
+    if (track.dataset.mouseDownAt === "0") return;
 
-    const mouseDelta = mouseDownAt - e.clientX,
-      maxDelta = window.innerWidth / 2;
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+      maxDelta = innerWidth / 2;
 
-    percentage = (mouseDelta / maxDelta) * -100;
-    let nextPercentageUnconstrained = prevPercentage + percentage;
-    nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+    const percentage = (mouseDelta / maxDelta) * -100,
+      nextPercentageUnconstrained =
+        parseFloat(track.dataset.prevPercentage) + percentage,
+      nextPercentage = Math.min(Math.max(nextPercentageUnconstrained, -100), 0);
+
+    track.dataset.percentage = nextPercentage;
 
     track.animate(
       {
-        transform: `translate(${nextPercentage}%, -50%)`,
+        transform: `translate(${nextPercentage}%)`,
       },
       { duration: 1200, fill: "forwards" }
     );
 
-    for (const image of track.getElementsByClassName("image")) {
+    for (const image of track.children) {
       image.animate(
         {
-          objectPosition: `${100 + nextPercentage}% center`,
+          objectPosition: `${100 + nextPercentage}%`,
         },
         { duration: 1200, fill: "forwards" }
       );
@@ -42,69 +41,64 @@
 </script>
 
 <svelte:window
+  bind:innerWidth={innerWidth}
   on:mousedown={handleOnDown}
-  on:mouseup={handleOnUp}
   on:mousemove={handleOnMove}
+  on:mouseup={handleOnUp}
 />
 <div
-  class="flex gap-16 absolute left-1/2 top-1/2 translate-y--1/2 translate-x-0 select-none"
+  class="flex gap-[4vmin] absolute left-1/2 top-1/4 select-none transform -translate-x-0 -translate-y-[-50%]"
   id="image-track"
   bind:this={track}
+  data-mouse-down-at="0"
+  data-prev-percentage="0"
 >
   <img
-    class="width-[40vmin] height-[56vmin] object-cover object-center"
+    class="w-[40vmin] h-[56vmin] object-cover object-[100%]"
     src="https://images.unsplash.com/photo-1524781289445-ddf8f5695861?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
     draggable="false"
     alt="a"
-    id="image"
   />
   <img
-    class="width-[40vmin] height-[56vmin] object-cover object-center"
+    class="w-[40vmin] h-[56vmin] object-cover object-[100%]"
     src="https://images.unsplash.com/photo-1610194352361-4c81a6a8967e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80"
     draggable="false"
     alt="a"
-    id="image"
   />
   <img
-    class="width-[40vmin] height-[56vmin] object-cover object-center"
+    class="w-[40vmin] h-[56vmin] object-cover object-[100%]"
     src="https://images.unsplash.com/photo-1618202133208-2907bebba9e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
     draggable="false"
     alt="a"
-    id="image"
   />
   <img
-    class="width-[40vmin] height-[56vmin] object-cover object-center"
+    class="w-[40vmin] h-[56vmin] object-cover object-[100%]"
     src="https://images.unsplash.com/photo-1495805442109-bf1cf975750b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
     draggable="false"
     alt="a"
-    id="image"
   />
   <img
-    class="width-[40vmin] height-[56vmin] object-cover object-center"
+    class="w-[40vmin] h-[56vmin] object-cover object-[100%]"
     src="https://images.unsplash.com/photo-1548021682-1720ed403a5b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
     draggable="false"
     alt="a"
-    id="image"
   />
   <img
-    class="width-[40vmin] height-[56vmin] object-cover object-center"
+    class="w-[40vmin] h-[56vmin] object-cover object-[100%]"
     src="https://images.unsplash.com/photo-1496753480864-3e588e0269b3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2134&q=80"
     draggable="false"
     alt="a"
-    id="image"
   />
   <img
-    class="width-[40vmin] height-[56vmin] object-cover object-center"
+    class="w-[40vmin] h-[56vmin] object-cover object-[100%]"
     src="https://images.unsplash.com/photo-1613346945084-35cccc812dd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1759&q=80"
     draggable="false"
     alt="a"
-    id="image"
   />
   <img
-    class="width-[40vmin] height-[56vmin] object-cover object-center"
+    class="w-[40vmin] h-[56vmin] object-cover object-[100%]"
     src="https://images.unsplash.com/photo-1516681100942-77d8e7f9dd97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
     draggable="false"
     alt="a"
-    id="image"
   />
 </div>
