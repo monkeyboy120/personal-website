938c6b19922176b647089d2262e9f4e1884a12c3
Joshua La <j0shua001@outlook.com>
2023-01-30 14:19:35 -0500

    Trying to animate navbar and changed read me

---
+++ src/components/WordCarousel.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  let target: HTMLElement;
+  function TextRotate({ target, data}) {
+    const text = target.data.rotate;
+    const rotate = target.data.rotate;
+    let index = 0;
+
+    setInterval(() => {
+      target.textContent = text + rotate[index];
+      index = (index + 1) % rotate.length;
+    }, 2000);
+  }
+
+</script>
+<div class="inline-block text-center animate-typewriter whitespace-nowrap overflow-hidden">
+<p class="text-8xl text-gray-600">
+  Welcome to My
+</p>
+
+<p class="text-8xl" data-rotate='["Website", "Project", "Portfolio", "Passion"]' bind:this={target}>
+  
+</p>
+</div>
