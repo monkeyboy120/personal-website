1c7aadecf852fd4fa0e4b535884141cb9c01d65c
Joshua La <j0shua001@outlook.com>
2023-02-01 13:01:03 -0500

    Working on blogcards idea

--- src/components/BlogCard.svelte
+++ src/components/BlogCard.svelte
@@ -2,11 +2,15 @@
     let container: HTMLElement;
     function createCard() {
         container.appendChild(document.createElement('div'));
+        container.children[container.children.length - 1].innerHTML = `
+            <div class="card">
+                <h2>Card Title</h2>
+                <p>Card Content</p>
+            </div>
+        `;
     }
 </script>
 
 <div class="grid columns-1" bind:this={container}>
-    <div>
-
-    </div>
+    <button on:click={createCard}>Add Card</button>
 </div>
