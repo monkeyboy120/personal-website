8d0b39b15f7b0344576c1d58fd4af560973ae74a
Joshua La <j0shua001@outlook.com>
2023-02-02 14:18:53 -0500

    fixed astro 2.0 update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,22 +6,8 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 
 // https://astro.build/config
-
-// https://astro.build/config
-
-// https://astro.build/config
-
-// https://astro.build/config
-import tailwind@latest from "@astrojs/tailwind";
-import svelte@latest from "@astrojs/svelte";
-
-// https://astro.build/config
 export default defineConfig({
-<<<<<<< HEAD
   integrations: [svelte(), tailwind()],
   output: "server",
   adapter: netlify()
-=======
-  integrations: [svelte(), tailwind(), tailwind@latest(), svelte@latest()]
->>>>>>> main
 });
