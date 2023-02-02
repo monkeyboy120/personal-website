7986478fa23949af27b1283ec072b77471f5f0d7
Joshua La <j0shua001@outlook.com>
2023-02-02 10:52:06 -0500

    Updated tailwind svelte and netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,10 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 
 // https://astro.build/config
+import tailwind@latest from "@astrojs/tailwind";
+import svelte@latest from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind(), tailwind@latest(), svelte@latest()]
 });
