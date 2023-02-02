9b00c260d5436d9fe6cf8aae296df8015265a0f3
Joshua La <j0shua001@outlook.com>
2023-02-02 10:49:42 -0500

    updated astro files

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,11 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 
 // https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  output: "server",
+  adapter: netlify()
 });
