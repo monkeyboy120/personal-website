a3b7cdf8425258076d48bbd885dc68b719fc83f1
Joshua La <j0shua001@outlook.com>
2023-02-03 12:10:25 -0500

    added some images and lightbox on photography page

---
+++ src/components/LightBox.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  let popup: HTMLDivElement;
+  function close() {
+    popup.style.display = "none";
+  }
+</script>
+
+<svelte:window on:click={close} />
+<div class="w-full h-full bg-slate-600 opacity-50 flex justify-center align-middle" bind:this={popup}>
+  <p class="text-4xl text-white absolute top-[5rem] left-1/2">Click and drag to view photo gallery</p>
+</div>
