d6d77d671dc39d8385cca40fe815fcf6ad3c1d0e
Joshua La <j0shua001@outlook.com>
2023-02-03 13:33:21 -0500

    got rid of errors in the word carousl and reworked popup message

--- src/components/LightBox.svelte
+++ src/components/LightBox.svelte
@@ -6,6 +6,7 @@
 </script>
 
 <svelte:window on:click={close} />
-<div class="w-full h-full bg-slate-600 opacity-50 flex justify-center align-middle" bind:this={popup}>
-  <p class="text-4xl text-white absolute top-[5rem] left-1/2">Click and drag to view photo gallery</p>
+<div class="w-full h-full bg-slate-600 opacity-50 flex justify-center " bind:this={popup}>
+  <p class="text-4xl text-white bg-amber-500 absolute top-1/2 left-4 cursor-vertical-text">Click and drag to view photo gallery</p>
 </div>
+
