3309b7da40a1de14b3dfb4215c2f8a2a3959c112
Joshua La <j0shua001@outlook.com>
2023-02-13 22:32:43 -0500

    WIP on dynamic routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,12 @@ import netlify from "@astrojs/netlify/functions";
 // https://astro.build/config
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  site: "http://joshuala.com",
+  integrations: [svelte(), tailwind(), sitemap()],
   output: "server",
   adapter: netlify()
 });
